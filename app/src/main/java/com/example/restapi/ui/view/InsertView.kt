445f14c76829e257705b0cf7dccb7f5c236028ff
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-12-28 23:15:04 +0700

    Menambahkan fun EntryBody

--- app/src/main/java/com/example/restapi/ui/view/InsertView.kt
+++ app/src/main/java/com/example/restapi/ui/view/InsertView.kt
@@ -1,19 +1,27 @@
 package com.example.restapi.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.restapi.ui.costumwidget.CostumeTopAppBar
 import com.example.restapi.ui.navigation.DestinasiNavigasi
+import com.example.restapi.ui.viewmodel.InsertUiEvent
+import com.example.restapi.ui.viewmodel.InsertUiState
 import com.example.restapi.ui.viewmodel.InsertViewModel
 import com.example.restapi.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
@@ -60,6 +68,34 @@ fun EntryMhsScreen(
         )
     }
 }
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+
 
 
 
