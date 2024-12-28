907845cd39d4d74d9038aad4d93d2542b9b22e00
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-12-28 23:09:56 +0700

    Menambahkan InsertUiState dan InsertUiEvent

--- app/src/main/java/com/example/restapi/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/restapi/ui/viewmodel/InsertViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.restapi.model.Mahasiswa
 import com.example.restapi.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
@@ -26,4 +27,39 @@ class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
         }
     }
 }
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
 
