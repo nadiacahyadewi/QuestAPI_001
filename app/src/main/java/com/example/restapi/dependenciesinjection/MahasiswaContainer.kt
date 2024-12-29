22a689e6e96dcf53624c1883b0d9001ac681822f
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-12-29 15:07:14 +0700

    Menambahkan MahasiswaContainer

---
+++ app/src/main/java/com/example/restapi/dependenciesinjection/MahasiswaContainer.kt
@@ -0,0 +1,28 @@
+package com.example.restapi.dependenciesinjection
+
+import com.example.restapi.repository.MahasiswaRepository
+import com.example.restapi.repository.NetworkKontakRepository
+import com.example.restapi.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer{
+    val kontakRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer: AppContainer{
+
+    private val baseUrl = "http://10.0.2.2:80/umyTI/"
+    private val json = Json{ignoreUnknownKeys = true}
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)}
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+    NetworkKontakRepository(mahasiswaService)}
+}
