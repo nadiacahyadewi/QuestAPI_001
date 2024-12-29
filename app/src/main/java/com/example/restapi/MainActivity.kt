31c0dba921191ebf74becdf0e2d06bbc8799d125
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-12-29 22:53:28 +0700

    memanggil mahasiswaApp di MainActivity

--- app/src/main/java/com/example/restapi/MainActivity.kt
+++ app/src/main/java/com/example/restapi/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.restapi.ui.theme.RestApiTheme
+import com.example.restapi.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             RestApiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MahasiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
